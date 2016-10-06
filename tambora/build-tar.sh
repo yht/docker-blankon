7eb77f2976bd619af1563f20de30841ccdaf92ca
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2016-10-06 21:23:25 +0700

    tambora fix build-tar

--- tambora/build-tar.sh
+++ tambora/build-tar.sh
@@ -1,4 +1,6 @@
 gpg --recv-key 91824AB09120A048
+ln -sf /usr/share/debootstrap/scripts/sid \
+   /usr/share/debootstrap/scripts/tambora
 debootstrap --keyring=~/.gpg/pubring.gpg --variant=minbase \
    --components=main --include=inetutils-ping,iproute2 \
    tambora tambora http://arsip.blankonlinux.or.id/blankon
